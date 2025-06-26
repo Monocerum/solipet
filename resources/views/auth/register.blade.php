4245b02c83ba10202cdabfe2c0f93564f7452db8
Monocerum <111045750+Monocerum@users.noreply.github.com>
2025-06-27 00:42:09 +0800

    feat: add responsive styles for register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,11 +37,13 @@
         h3 {
             font-size: 2.1em;
             margin: 0;
+            text-align: end;
         }
 
         h5 {
             font-size: 1.7em;
             margin: 0;
+            text-align: end;
         }
     }
 
@@ -56,7 +58,7 @@
     .auth-card-header {
         font-family: "Irish Grover", sans-serif;
         font-size: 50px;
-        padding: 5% 0 0 5%;
+        padding: 5% 0;
         color: #E8C7AA;
         text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     }
@@ -122,6 +124,82 @@
             }
         }
     }
+
+    @media screen and (max-width: 1450px) {
+        .auth-card {
+            background: #77401E;
+            color: white;
+            width: 40em;
+            min-height: 50vh;
+            border-radius: 1.5em;
+        }
+    }
+
+    @media screen and (max-width: 1000px) {
+        .auth-card {
+            width: 30em;
+        }
+
+        .solipet-tagline {
+            h3 {
+                font-size: 1.5em;
+            }
+
+            h5 {
+                font-size: 1.2em;
+            }
+        }
+    }
+
+    @media screen and (max-width: 870px) {
+        .register-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            .solipet-tagline-container {
+                height: 10vh;
+                display: flex;
+                padding-right: 0;
+                align-items: center;
+
+                .solipet-tagline {
+                    align-items: center;
+                }
+
+                h3, h5 {
+                    text-align: center;
+                }
+            }
+        }
+    }
+
+    @media screen and (max-width: 550px) {
+        .auth-card {
+            width: 25em;
+        }
+
+        .auth-card-body {
+            padding: 10%;
+        }
+    }
+
+    @media screen and (max-width: 550px) {
+        .auth-card {
+            width: 20em;
+        }
+
+        .auth-card-header {
+            font-size: 3em;
+        }
+
+        .redirect-container {
+            p {
+                text-align: center;
+                font-size: 1em;
+            }
+        }
+    }
 </style>
 
 <div class="container register-container">
@@ -133,11 +211,9 @@
     </div>
     <div class="register-card-container">
         <div class="auth-card solipet-register">
-            <div>
                 <div class="card auth-card">
-                    <div class="auth-card-header">{{ __('REGISTER') }}</div>
-
                     <div class="card-body auth-card-body">
+                        <h2 class="auth-card-header">{{ __('REGISTER') }}</h2>
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
 
@@ -219,7 +295,6 @@
                         </form>
                     </div>
                 </div>
-            </div>
         </div>
     </div>
 </div>
