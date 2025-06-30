c3fafc42227896e80de250b71d5a1c2983d4172b
Bryll Soliven <princebryll.soliven8@gmail.com>
2025-06-30 14:52:15 +0800

    change phone to phonenumber

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
             'username' => 'required|string|max:255|unique:users,username,' . $user->id,
             'name' => 'nullable|string|max:255',
             'email' => 'required|email|max:255|unique:users,email,' . $user->id,
-            'phone' => 'nullable|string|max:20',
+            'phonenumber' => 'nullable|string|max:20',
             'gender' => 'nullable|in:male,female,other',
             'day' => 'nullable|integer|min:1|max:31',
             'month' => 'nullable|integer|min:1|max:12',
